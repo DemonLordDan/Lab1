1c2c178c67397729fd8f1a20d8fe89066cbacfa9
DemonLordDan <63431508+DemonLordDan@users.noreply.github.com>
2021-03-09 15:14:32 +0000

    CMAKE Problems fix

--- SpaceInvaders/main.cpp
+++ SpaceInvaders/main.cpp
@@ -1,1 +1,33 @@
+//Main.cpp
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+Texture spritesheet;
+Sprite invader;
+
+void Load() {
+    if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
+        cerr << "Failed to load spritesheet!" << std::endl;
+    }
+    invader.setTexture(spritesheet);
+    invader.setTextureRect(sf::IntRect(0, 0, 32, 32));
+};
+
+void Render(RenderWindow& window) {
+    window.draw(invader);
+};
+
+int main() {
+    RenderWindow window(sf::VideoMode(128, 128), "SFML works!");
+    Load();
+    while (window.isOpen()) {
+        window.clear();
+        //Update(window);
+        Render(window);
+        window.display();
+    }
+    return 0;
+}
 

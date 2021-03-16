18d933f28eec4459bd37d5030a110e00d2e98cbf
DemonLordDan <63431508+DemonLordDan@users.noreply.github.com>
2021-03-16 13:04:24 +0000

    More Space Invaders

--- SpaceInvaders/main.cpp
+++ SpaceInvaders/main.cpp
@@ -1,27 +1,38 @@
 //Main.cpp
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "ship.h"
+#include "game.h"
+
 
 using namespace std;
 using namespace sf;
 
+
+std::vector<Ship*> ships;
+
 sf::Texture spritesheet;
-sf::Sprite invader;
+//sf::Sprite invader;
 
 void Load() {
     if (!spritesheet.loadFromFile("res/img/invaders_sheet.png")) {
         cerr << "Failed to load spritesheet!" << std::endl;
     }
-    invader.setTexture(spritesheet);
-    invader.setTextureRect(sf::IntRect(0, 0, 32, 32));
+    Invader* inv = new Invader(sf::IntRect(0, 0, 32, 32), { 100,100 });
+    ships.push_back(inv);
 };
 
 void Render(RenderWindow& window) {
-    window.draw(invader);
+    for (int i = 0; i < ships.size(); i++) {
+    ships[i];
+    }
 };
 
+
 int main() {
-    RenderWindow window(sf::VideoMode(128, 128), "SFML works!");
+    Clock clock;
+    float dt = clock.restart().asSeconds();
+    RenderWindow window(sf::VideoMode(gameWidth, gameHeight), "SFML works!");
     Load();
     while (window.isOpen()) {
         window.clear();
---
+++ SpaceInvaders/ship.cpp
@@ -0,0 +1,29 @@
+//ship.cpp
+#include "ship.h"
+using namespace sf;
+using namespace std;
+
+Ship::Ship() {};
+
+Ship::Ship(IntRect ir) : Sprite() {
+	_sprite = ir;
+	setTexture(spritesheet);
+	setTextureRect(_sprite);
+};
+
+void Ship::Update(const float& dt) {}
+
+Invader::Invader() : Ship() {}
+
+Invader::Invader(sf::IntRect ir, sf::Vector2f pos) : Ship(ir) {
+	setOrigin(16, 16);
+	setPosition(pos);
+}
+
+void Invader::Update(const float& dt) {
+	Ship::Update(dt);
+}
+
+//Define the ship deconstructor. 
+//Although we set this to pure virtual, we still have to define it.
+Ship::~Ship() = default;
